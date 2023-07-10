f991cb9c2a8159b72cf30ad4d13c7ec7bee1c27f
hannahker <hannah.ker96@gmail.com>
2023-07-10 14:45:12 -0700

    Add boilerplate to set up werkzeug profiler

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import os
 from dash import Dash
 import dash_mantine_components as dmc
+from werkzeug.middleware.profiler import ProfilerMiddleware
 from components.control_bar import layout as control_bar_layout
 from components.image_viewer import layout as image_viewer_layout
 from callbacks.image_viewer import *
@@ -20,5 +22,16 @@ app.layout = dmc.MantineProvider(
     ],
 )
 
+PROF_DIR = "prof"
+
 if __name__ == "__main__":
+    if os.getenv("PROFILER", None):
+        app.server.config["PROFILE"] = True
+        app.server.wsgi_app = ProfilerMiddleware(
+            app.server.wsgi_app,
+            sort_by=["cumtime"],
+            restrictions=[50],
+            stream=None,
+            profile_dir=PROF_DIR,
+        )
     app.run_server(debug=True)
