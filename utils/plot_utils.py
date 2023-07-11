e25c3cb1079f15f1b95deea401a822cca21d2e6a
hannahker <hannah.ker96@gmail.com>
2023-07-11 13:51:28 -0700

    💄 Improve default chart view

---
+++ utils/plot_utils.py
@@ -0,0 +1,10 @@
+import plotly.graph_objects as go
+
+
+def blank_fig():
+    fig = go.Figure(go.Scatter(x=[], y=[]))
+    fig.update_layout(template=None)
+    fig.update_xaxes(showgrid=False, showticklabels=False, zeroline=False)
+    fig.update_yaxes(showgrid=False, showticklabels=False, zeroline=False)
+
+    return fig
