3d37ca1e76657eeeebd9a66c915098e34e79b11d
hannahker <hannah.ker96@gmail.com>
2023-07-28 08:46:20 -0700

    test with empty children

--- components/control_bar.py
+++ components/control_bar.py
@@ -358,23 +358,23 @@ def layout():
                             dmc.Space(h=20),
                         ],
                     ),
-                    # _accordion_item(
-                    #     "Model configuration",
-                    #     "carbon:ibm-watson-machine-learning",
-                    #     "run-model",
-                    #     children=[
-                    #         dmc.Center(
-                    #             dmc.Button(
-                    #                 children="Run model",
-                    #                 id="run-model",
-                    #                 variant="light",
-                    #                 style={"width": "160px", "margin": "5px"},
-                    #             )
-                    #         ),
-                    #         dmc.Center(html.Div(id="output-placeholder")),
-                    #         dmc.Space(h=20),
-                    #     ],
-                    # ),
+                    _accordion_item(
+                        "Model configuration",
+                        "carbon:ibm-watson-machine-learning",
+                        "run-model",
+                        children=[
+                            # dmc.Center(
+                            #     dmc.Button(
+                            #         children="Run model",
+                            #         id="run-model",
+                            #         variant="light",
+                            #         style={"width": "160px", "margin": "5px"},
+                            #     )
+                            # ),
+                            # dmc.Center(html.Div(id="output-placeholder")),
+                            # dmc.Space(h=20),
+                        ],
+                    ),
                 ],
             ),
             dcc.Store(
