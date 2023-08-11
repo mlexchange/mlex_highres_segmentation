6d40adde08b1fd19ed8b39cb5a4bae26ab24cccc
admin <51248046+danton267@users.noreply.github.com>
2023-08-11 13:47:07 +0100

    🐛 change alt to ctrl

--- components/control_bar.py
+++ components/control_bar.py
@@ -563,7 +563,7 @@ def drawer_section(children):
                 events=[
                     {
                         "event": "keydown",
-                        "props": ["key", "altKey", "altKey"],
+                        "props": ["key", "ctrlKey", "ctrlKey"],
                     }
                 ],
                 id="keybind-event-listener",
@@ -650,7 +650,7 @@ def create_info_card_affix():
                                     "Select annotation class 1-9",
                                 ),
                                 create_keybind_row(
-                                    ["alt (⌥ option)", "1-9"],
+                                    ["clrl (control)", "1-9"],
                                     "Select annotation class 10-19",
                                 ),
                             ],
