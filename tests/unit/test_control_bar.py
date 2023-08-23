c52a744c658b7e381505c4e7ce75b6d9e8293349
hannahker <hannah.ker96@gmail.com>
2023-08-23 12:10:41 -0700

    ✅ WIP add mock for Tiled

--- tests/unit/test_control_bar.py
+++ tests/unit/test_control_bar.py
@@ -1,5 +1,12 @@
 from callbacks.control_bar import reset_filters
 import pytest
+import numpy as np
+
+class TiledDataMock:
+    def __init__(self):
+        self["sample_project"] = np.zeros(500, 500, 500)
+
+
 
 
 @pytest.fixture
@@ -8,6 +15,7 @@ def nclicks():
 
 
 def test_reset_filters(nclicks):
+    mocker.patch("constants.DATA", new=TiledDataMock())
     default_brightness, default_contrast = reset_filters(nclicks)
     assert default_brightness == 100
     assert default_contrast == 100
