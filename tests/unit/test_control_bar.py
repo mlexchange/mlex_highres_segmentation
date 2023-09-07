496251735d3ae41ac3f576ec74b55be430533d39
hannahker <hannah.ker96@gmail.com>
2023-09-07 10:55:45 -0700

    🐛 switch to fixture instead of mock

--- tests/unit/test_control_bar.py
+++ tests/unit/test_control_bar.py
@@ -1,11 +1,10 @@
-from callbacks.control_bar import reset_filters
 import pytest
 import numpy as np
 
 
-class TiledDataMock:
-    def __init__(self):
-        self["sample_project"] = np.zeros(500, 500, 500)
+@pytest.fixture
+def tiled_data_mock():
+    return {"sample_project": np.zeros((500, 500, 500)), "reconstruction": 0}
 
 
 @pytest.fixture
@@ -13,6 +12,10 @@ def nclicks():
     return True
 
 
-def test_reset_filters(nclicks, mocker):
-    mocker.patch("utils.data_utils.client", new=TiledDataMock())
+def test_reset_filters(mocker, nclicks, tiled_data_mock):
+    mocker.patch(
+        "tiled.client.from_uri",
+        return_value=tiled_data_mock,
+    )
+    from callbacks.control_bar import reset_filters
     assert reset_filters(nclicks) == 100
