f6dd0540f0e4a35d82d1ccb34208a304d676a523
Clea Aumont <clea.aumont@plot.ly>
2023-10-03 14:46:00 -0400

    updating condition for prevent update

--- callbacks/image_viewer.py
+++ callbacks/image_viewer.py
@@ -114,7 +114,10 @@ def render_image(
         if toggle_seg_result:
             # if toggle is true and overlay exists already (2 images in data) this will
             # be handled in hide_show_segmentation_overlay callback
-            if len(fig["data"]) == 2 and ctx.triggered_id != "image-selection-slider":
+            if (
+                len(fig["data"]) == 2
+                and ctx.triggered_id == "show-result-overlay-toggle"
+            ):
                 raise PreventUpdate
             result = get_data_sequence_by_name(seg_result_selection)[image_idx]
     else:
