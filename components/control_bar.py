4d351502e88f73a2510f392cb21ca3dd70f680b7
Wiebke Köpp <wiebke.koepp@googlemail.com>
2023-12-13 21:51:10 -0800

    Remove obsolete import of dash_core_components

--- components/control_bar.py
+++ components/control_bar.py
@@ -3,7 +3,6 @@ import dash_mantine_components as dmc
 from dash import dcc, html
 from dash_extensions import EventListener
 from dash_iconify import DashIconify
-import dash_core_components as dcc
 from components.annotation_class import annotation_class_item
 from constants import ANNOT_ICONS, KEYBINDS
 from utils import data_utils
