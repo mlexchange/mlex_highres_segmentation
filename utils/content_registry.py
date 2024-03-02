0a51ed006873a81bbcd9235aadffc7860f810d65
zhuowenzhao <zwenzhao11@gmail.com>
2024-03-01 16:13:30 -0800

    :sparkles: added content registry example

---
+++ utils/content_registry.py
@@ -0,0 +1,30 @@
+import json
+from copy import deepcopy
+
+class Models:
+    def __init__(self, modelfile_path='./assets/mode_description.json'):
+        self.path =  modelfile_path
+        f = open('./assets/mode_description.json')
+        
+        self.contents = json.load(f)['contents']
+        self.modelname_list = [content['model_name'] for content in self.contents]
+        self.models = {}
+
+        for i, n in enumerate(self.modelname_list):
+            self.models[n] = self.contents[i]
+    
+    @staticmethod
+    def remove_key_from_dict_list(data, key):
+        new_data = []
+        for item in data:
+            if key in item:
+                new_item = deepcopy(item)
+                new_item.pop(key)
+                new_data.append(new_item)
+            else:
+                new_data.append(item)
+        
+        return new_data 
+
+
+models = Models()
