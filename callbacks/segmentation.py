212530d7cf5832938fb56517fbb9783841502d9c
Wiebke Köpp <wiebke.koepp@googlemail.com>
2024-03-02 19:41:17 -0800

    Capture `mask_uri` for job submission

--- callbacks/segmentation.py
+++ callbacks/segmentation.py
@@ -74,19 +74,19 @@ def run_job(n_clicks, global_store, all_annotations, project_name):
     """
     if n_clicks:
         if MODE == "dev":
-            tiled_masks.save_annotations_data(
+            mask_uri = tiled_masks.save_annotations_data(
                 global_store, all_annotations, project_name
             )
             job_uid = str(uuid.uuid4())
             return (
                 dmc.Text(
-                    f"Workflow has been succesfully submitted with uid: {job_uid}",
+                    f"Workflow has been succesfully submitted with uid: {job_uid} and mask uri: {mask_uri}",
                     size="sm",
                 ),
                 job_uid,
             )
         else:
-            tiled_masks.save_annotations_data(
+            mask_uri = tiled_masks.save_annotations_data(
                 global_store, all_annotations, project_name
             )
             job_submitted = requests.post(
