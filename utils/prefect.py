ef72374bda3af33335db07f9ca8308d43dad63de
Wiebke Köpp <wiebke.koepp@googlemail.com>
2024-05-14 19:17:21 -0700

    :sparkles: Check Prefect API Status

--- utils/prefect.py
+++ utils/prefect.py
@@ -10,6 +10,31 @@ from prefect.client.schemas.filters import (
 )
 
 
+async def _check_prefect_ready():
+    async with get_client() as client:
+        healthcheck_result = await client.api_healthcheck()
+        if healthcheck_result is not None:
+            raise Exception("Prefect API is not healthy.")
+
+
+def check_prefect_ready():
+    return asyncio.run(_check_prefect_ready())
+
+
+async def _check_prefect_worker_ready(deployment_name: str):
+    async with get_client() as client:
+        deployment = await client.read_deployment_by_name(deployment_name)
+        assert (
+            deployment
+        ), f"No deployment found in config for deployment_name {deployment_name}"
+        if deployment.status != "Ready":
+            raise Exception("Deployment used for training and inference is not ready.")
+
+
+def check_prefect_worker_ready(deployment_name: str):
+    return asyncio.run(_check_prefect_worker_ready(deployment_name))
+
+
 async def _schedule(
     deployment_name: str,
     flow_run_name: str,
