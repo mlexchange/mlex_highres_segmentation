29952037239029ddcf3a6a643762ab5242c5c445
Xiaoya Chong <150726549+xiaoyachong@users.noreply.github.com>
2025-11-11 19:10:36 -0800

    add algorithm registry

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -9,6 +9,7 @@ from tiled.client import from_uri
 from tiled.client.array import ArrayClient
 from tiled.client.container import Container
 
+from mlex_utils.mlflow_utils.mlflow_algorithm_client import MlflowAlgorithmClient
 from utils.annotations import Annotations
 
 load_dotenv()
@@ -20,6 +21,10 @@ MASK_TILED_API_KEY = os.getenv("MASK_TILED_API_KEY")
 SEG_TILED_URI = os.getenv("SEG_TILED_URI")
 SEG_TILED_API_KEY = os.getenv("SEG_TILED_API_KEY")
 USER_NAME = os.getenv("USER_NAME", "user1")
+
+MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", "http://mlflow:5000")
+MLFLOW_TRACKING_USERNAME = os.getenv("MLFLOW_TRACKING_USERNAME", "")
+MLFLOW_TRACKING_PASSWORD = os.getenv("MLFLOW_TRACKING_PASSWORD", "")
 
 
 def _create_or_return_containers(client, container_names):
@@ -357,20 +362,28 @@ tiled_results = TiledDataLoader(
 
 
 class Models:
-    def __init__(self, modelfile_path="./assets/models.json"):
-        self.path = modelfile_path
-        f = open(self.path)
+    """
+    This class loads algorithm definitions from MLflow instead of a local JSON file.
+    """
 
-        contents = json.load(f)["contents"]
-        self.modelname_list = [content["model_name"] for content in contents]
-        self.models = {}
+    def __init__(self):
+        # Initialize MLflow client
+        self.mlflow_client = MlflowAlgorithmClient(
+            MLFLOW_TRACKING_URI,
+            MLFLOW_TRACKING_USERNAME,
+            MLFLOW_TRACKING_PASSWORD,
+        )
 
-        for i, n in enumerate(self.modelname_list):
-            self.models[n] = contents[i]
+        # Load algorithms from MLflow filtered by type="segmentation"
+        self.mlflow_client.load_from_mlflow(algorithm_type="segmentation")
+
+        # Get list of model names
+        self.modelname_list = self.mlflow_client.modelname_list
 
     def __getitem__(self, key):
+        """Get model by name from MLflow"""
         try:
-            return self.models[key]
+            return self.mlflow_client[key]
         except KeyError:
             raise KeyError(f"A model with name {key} does not exist.")
 
@@ -389,7 +402,7 @@ def extract_parameters_from_html(model_parameters_html):
         # param["props"]["children"][0] is the label
         # param["props"]["children"][1] is the input
         parameter_container = param["props"]["children"][1]
-        # The achtual parameter item is the first and only child of the parameter container
+        # The actual parameter item is the first and only child of the parameter container
         parameter_item = parameter_container["props"]["children"]["props"]
         key = parameter_item["id"]["param_key"]
         if "value" in parameter_item:
